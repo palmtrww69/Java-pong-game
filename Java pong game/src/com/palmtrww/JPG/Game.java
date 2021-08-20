14e61f782e71b6a1b9971632c4c68c4109ec7504
palmtrww69 <palmtrwwemail@yandex.com>
2021-08-19 21:41:21 -0700

    code

---
+++ Java pong game/src/com/palmtrww/JPG/Game.java
@@ -0,0 +1,16 @@
+package com.palmtrww.JPG;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Game {
+
+	public static void main(String[] args) {
+		
+		GameFrame frame = new GameFrame();
+
+	}
+
+}
